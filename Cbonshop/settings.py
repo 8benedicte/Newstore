2c25beb51d69e91cc60ab249bdeaa7a65e5ac9ef
8benedicte <benedicteg28@gmail.com>
2023-12-18 17:50:04 +0000

    currencies suppression

--- Cbonshop/settings.py
+++ Cbonshop/settings.py
@@ -72,7 +72,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',                 'accounts.context_processors.user_context',                 'currencies.context_processors.currencies',  
             ],
         },
     },
